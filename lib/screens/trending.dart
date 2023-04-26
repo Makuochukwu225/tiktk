338586df8a7919133956860e37897c2de3e11801
Collins Mark <87154745+Makuochukwu225@users.noreply.github.com>
2023-04-26 07:02:59 +0100

    initial

---
+++ lib/screens/trending.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:tiktok/models/tiktok.dart';
+import 'package:tiktok/config/api.dart';
+import 'package:http/http.dart' as http;
+import 'package:tiktok/screens/tiktokvideo.dart';
+
+class Trending extends StatefulWidget {
+  @override
+  _TrendingState createState() => _TrendingState();
+}
+
+class _TrendingState extends State<Trending> {
+  PageController pageController = PageController();
+  RequestController api = RequestController();
+  List<Widget> tikTokVideos = [];
+
+  getTrending() async {
+    var cookies = await api.getCookie();
+    api.setCookie(cookies);
+    try {
+      var response = await http.get(
+        Uri.parse(api.url),
+        headers: api.headers,
+      );
+      Tiktok tiktok = Tiktok.fromJson(jsonDecode(response.body));
+      tiktok.body!.itemListData!.forEach(
+        (item) {
+          setState(() {
+            tikTokVideos.add(TikTokVideo(data: item));
+          });
+        },
+      );
+    } catch (ex) {
+      print(ex);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTrending();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      scrollDirection: Axis.vertical,
+      controller: pageController,
+      children: tikTokVideos.length == 0
+          ? <Widget>[
+              Container(
+                color: Colors.black,
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              )
+            ]
+          : tikTokVideos,
+    );
+  }
+}
